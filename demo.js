b596a8e401b6a4318ee67e4cb7e8f9c3d05533fc
reikoy@163.com <1587720854@qq.com>
2020-05-04 15:34:31 +0800

    优化

---
+++ demo.js
@@ -0,0 +1,25 @@
+
+function a(el){
+    let par=el.offsetParent
+    let top=el.offsetTop;
+    while (par&&par.tagName!=='BODY') {
+        top+=par.clientTop
+        top+=par.offsetTop
+        par = par.offsetParent
+    }
+    return top
+}
+window.addEventListener('scroll',function(){
+    let top=a(el)
+    console.log(top)
+    let clientW=document.documentElement.clientHeight
+
+    let clientT=document.documentElement.scrollTop
+    console.log(clientT+clientW)
+    if (top+el.clientHeight>=clientT&& top<=clientT+clientW) {
+        console.log('进来了')
+    }else {
+        console.log('出去了')
+    }
+
+})
